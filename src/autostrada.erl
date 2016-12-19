06f5d1bf3de92690a0b2edd9e22e80c0463e2443
Dominik Baran <dominik.baran7@gmail.com>
2016-12-19 21:20:22 +0100

    Changes

--- src/autostrada.erl
+++ src/autostrada.erl
@@ -10,36 +10,55 @@
 -define(BLACK_FG, 6).
 -define(CARCOLOR_START, 7).
 -define(CARCOLOR_END, 14).
--define(WHITE, 15).
+-define(BLACK, 15).
+-define(FONT_COLOR, 16).
 
 -define(ROAD_WIDTH, 8).
 -define(CAR_HEIGHT, 4).
 -define(GATE_HEIGHT, 3).
 -define(ROAD_COUNT, 8).
 -define(GATE_TIMEOUT, 2000).
--define(CARSPAWNER_TIMEOUT, 500).
+-define(ONEHOUR, 5000).
+-define(DRAWSPEED, 50).
 
 -record(car, {speed, pid, color, y}).
-%-record(road, {window = 0, reverse = false, cars = []}).
 
 main() ->
 try
   init(),
   Roads = createRoads(),
   initCarSpawner(Roads),
-  drawLoop(Roads)
+  drawLoop(Roads, 0)
 after
   application:stop(cecho)
 end.
 
-drawLoop(Roads) ->
-  cecho:wbkgd(?ceSTDSCR, ?ceCOLOR_PAIR(?WHITE)),
+drawLoop(Roads, Hour) ->
+  receive {From, getHour} -> From ! Hour after 1 -> nothing end,
+  cecho:wbkgd(?ceSTDSCR, ?ceCOLOR_PAIR(?BLACK)),
   cecho:wnoutrefresh(?ceSTDSCR),
   Fn  = fun(Pid) -> syncMessage(Pid, draw) end,
   lists:foreach(Fn, Roads),
+  drawStats(Hour, Roads),
   cecho:doupdate(),
-  timer:sleep(40),
-  drawLoop(Roads).
+  timer:sleep(?DRAWSPEED),
+  drawLoop(Roads, Hour + ?DRAWSPEED).
+
+drawStats(Hour, Roads) ->
+  DisplayHour = (Hour div ?ONEHOUR) rem 24,
+  cecho:attron(?ceSTDSCR, ?ceCOLOR_PAIR(?FONT_COLOR)),
+  cecho:mvaddstr(0, 0, io_lib:format("Godzina: ~p", [DisplayHour])),
+
+  Fun = fun (Pid) -> Pid ! {self(), getCarCount}, receive Count -> Count end end,
+  CarsCount = lists:sum(lists:map(Fun, Roads)),
+  cecho:mvaddstr(1, 0, io_lib:format("Liczba pojazdow: ~p", [CarsCount])),
+
+  cecho:attroff(?ceSTDSCR, ?ceCOLOR_PAIR(?FONT_COLOR)),
+  cecho:wnoutrefresh(?ceSTDSCR).
+
+ calculateCarCount(Pid) ->
+   Pid ! {self(), getCarCount},
+   receive X -> X end.
 
 syncMessage(Pid, Message) ->
     Pid ! {self(), Message},
@@ -58,7 +77,7 @@ init() ->
   cecho:init_pair(?ROAD_COLOR, 239, 239),
   cecho:init_pair(?ROAD_COLOR_LINE, 255, 239),
   cecho:init_pair(?BLACK_FG, 0, 239),
-  cecho:init_pair(?WHITE, 255, 255),
+  cecho:init_pair(?BLACK, 0, 0),
   cecho:init_pair(?GATE_COLOR, 35, 35),
   cecho:init_pair(?CARCOLOR_START, 61, 61),
   cecho:init_pair(?CARCOLOR_START + 1, 133, 133),
@@ -68,15 +87,17 @@ init() ->
   cecho:init_pair(?CARCOLOR_START + 5, 88, 88),
   cecho:init_pair(?CARCOLOR_START + 6, 97, 97),
   cecho:init_pair(?CARCOLOR_START + 7, 217, 217),
-  cecho:init_pair(?GATE_OPEN_COLOR, 34, 34).
+  cecho:init_pair(?GATE_OPEN_COLOR, 34, 34),
+  cecho:init_pair(?FONT_COLOR, 0, 255).
 
-initCarSpawner(Roads) ->spawn_link(?MODULE, carSpawnerProcess, [Roads]).
+initCarSpawner(Roads) -> spawn_link(?MODULE, carSpawnerProcess, [self(), Roads]).
 
 % processes
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 roadProcess(Window, Cars, WinHeight) ->
   IsReversed = isReversedRoad(Window),
   receive
+    {From, getCarCount} -> From ! length(Cars), roadProcess(Window, Cars, WinHeight);
     {From, draw} ->
       drawRoad(Window, Cars),
       From ! {self(), ok},
@@ -103,7 +124,6 @@ roadProcess(Window, Cars, WinHeight) ->
 
 carProcess(RoadPid, Car, WinHeight, IsReversed) ->
   timer:sleep(Car#car.speed * 100),
-  %timer:sleep(1000),
   UpdatedCar = updateCar(Car),
   IsCarPassingThruGate = willCarPassThruGate(UpdatedCar, WinHeight, IsReversed),
   if IsCarPassingThruGate -> timer:sleep(?GATE_TIMEOUT); true -> ok end,
@@ -114,12 +134,16 @@ carProcess(RoadPid, Car, WinHeight, IsReversed) ->
     end_journey -> terminate
   end.
 
-carSpawnerProcess(Roads) ->
-  Index = crypto:rand_uniform(1, length(Roads) + 1),
+carSpawnerProcess(MainPid, Roads) ->
+  Index =  rand:uniform(length(Roads)),
+  %Index = crypto:rand_uniform(1, length(Roads) + 1),
   Road = lists:nth(Index,Roads),
   Road ! {new_car, #car{speed=5,pid=0,color=randColor(),y=-?CAR_HEIGHT}},
-  timer:sleep(?CARSPAWNER_TIMEOUT),
-  carSpawnerProcess(Roads).
+  MainPid ! {self(), getHour},
+  Hour = (( receive H -> H end) div ?ONEHOUR) rem 24,
+  Timeout = if Hour =< 5 -> 5000; Hour =< 10 -> 800; Hour =< 15 -> 1000; Hour =< 18 -> 300; Hour =< 21 -> 1500; true -> 4000 end,
+  timer:sleep(Timeout),
+  carSpawnerProcess(MainPid, Roads).
 
 createRoads() -> lists:map(fun createRoad/1, lists:seq(1, ?ROAD_COUNT)).
 createRoad(Index) ->
@@ -130,7 +154,7 @@ createRoad(Index) ->
 updateCar(Car) -> Car#car{y=Car#car.y+1}.
 
 % helpers
-randColor() -> crypto:rand_uniform(?CARCOLOR_START, ?CARCOLOR_END + 1).
+randColor() -> ?CARCOLOR_START - 1 + rand:uniform(?CARCOLOR_END - ?CARCOLOR_START + 1).
 isReversedRoad(Window) -> Window > (?ROAD_COUNT div 2).
 validMove(_, []) -> true;
 validMove(NewCar, [#car{y=Y}|Cars]) -> if (NewCar#car.y + ?CAR_HEIGHT < Y) or (NewCar#car.y > Y) -> validMove(NewCar, Cars); true -> false end.
@@ -202,15 +226,3 @@ drawCar(Window, Height, #car{y=Y, color=Color}) ->
     cecho:delwin(SubWin);
   true -> ok
   end.
-  %cecho:attron(Window, ?ceCOLOR_PAIR(8)),
-  %cecho:mvwvline(Window, DrawY, 1, 0, CarHeight).
-  %cecho:mvwvline(Window, DrawY, ?ROAD_WIDTH - 3, 0, CarHeight),
-  %cecho:mvwhline(Window, DrawY + CarHeight, 1, 0, ?ROAD_WIDTH - 3),
-  %if NewY >= 0 ->
-    %cecho:mvwhline(Window, DrawY, 1, 0, ?ROAD_WIDTH - 3),
-    %cecho:corner(Window, DrawY, 1, 1),
-    %cecho:corner(Window, DrawY, ?ROAD_WIDTH - 3, 2);
-    %true -> ok
-  %end,
-  %cecho:corner(Window, DrawY + CarHeight, ?ROAD_WIDTH - 3, 3),
-  %cecho:corner(Window, DrawY + CarHeight, 1, 4).
